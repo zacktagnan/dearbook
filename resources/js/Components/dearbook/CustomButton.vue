45fa8186aabbc6704c0deef8aa4a7b2b7e7e8c1e
zacktagnan <zacktagnan@gmail.com>
2024-06-25 12:16:37 +0200

    CustomButton-ImplementaciOn.bAsica.inicial

---
+++ resources/js/Components/dearbook/CustomButton.vue
@@ -0,0 +1,32 @@
+<script setup>
+const props = defineProps({
+    type: {
+        type: String,
+        default: 'button',
+    },
+    generalClasses: {
+        type: String,
+        default: '',
+    },
+    colorClasses: {
+        type: String,
+        default: 'text-indigo-900 bg-indigo-100 hover:bg-indigo-200 focus-visible:ring-indigo-500',
+    },
+    otherClasses: {
+        type: String,
+        default: '',
+    },
+})
+
+const classes = ' ' + props.generalClasses + ' ' + props.colorClasses + ' ' + props.otherClasses
+</script>
+
+<template>
+    <button
+        :type="type"
+        class="inline-flex items-center justify-center px-4 py-2 text-sm font-medium border border-transparent rounded-md focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2"
+        :class="classes"
+    >
+        <slot />
+    </button>
+</template>
