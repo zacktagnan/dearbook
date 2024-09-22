950c5e4e096b184d96bc93cef79b6f5333545776
zacktagnan <zacktagnan@gmail.com>
2024-09-22 20:33:45 +0200

    Profile.Edit-Quitar.HEADER.Acerca_de

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -14,25 +14,9 @@ defineProps({
 </script>
 
 <template>
-    <div class="mt-2 space-y-6">
-        <div
-            class="px-4 bg-white shadow md:px-8 sm:py-4 dark:bg-gray-800 sm:rounded-lg"
-        >
-            <h2
-                class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200"
-            >
-                {{ $t("About") }}
-            </h2>
-        </div>
-    </div>
-
-    <div class="mt-4 space-y-6">
+    <div class="my-6 space-y-6">
         <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
-            <UpdateProfileInformationForm
-                :must-verify-email="mustVerifyEmail"
-                :status="status"
-                class="max-w-xl"
-            />
+            <UpdateProfileInformationForm :must-verify-email="mustVerifyEmail" :status="status" class="max-w-xl" />
         </div>
 
         <div class="p-4 bg-white shadow sm:p-8 dark:bg-gray-800 sm:rounded-lg">
