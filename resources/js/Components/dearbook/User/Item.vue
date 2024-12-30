f9e2116ea001f4a41e5132c6043c09ee36f5a771
zacktagnan <zacktagnan@gmail.com>
2024-12-30 18:44:45 +0100

    UserItem-Cambio.BG_Color.en.IMGs.avatar

--- resources/js/Components/dearbook/User/Item.vue
+++ resources/js/Components/dearbook/User/Item.vue
@@ -18,14 +18,15 @@ defineProps({
     <div :class="'rounded-md transition-all' + classes">
         <div :href="route('profile.index', { username: user.username })" class="flex items-center gap-2 px-2 py-1">
             <Link :href="route('profile.index', { username: user.username })">
-            <img :src="user.avatar_url" class="w-[36px] rounded-full" :alt="user.name">
+            <img :src="user.avatar_url" class="w-[36px] rounded-full bg-gray-200 dark:bg-gray-400" :alt="user.name">
             </Link>
             <div class="flex justify-between items-center flex-1">
                 <div class="flex-col leading-none">
                     <Link :href="route('profile.index', { username: user.username })">
                     <h3 class="font-black hover:underline">{{ user.name }}</h3>
                     </Link>
-                    <span v-if="userSinceDate" class="text-xs text-gray-500 dark:text-gray-300">{{ userSinceDate }}</span>
+                    <span v-if="userSinceDate" class="text-xs text-gray-500 dark:text-gray-300">{{ userSinceDate
+                        }}</span>
                 </div>
 
                 <div class="flex gap-1 md:gap-2">
