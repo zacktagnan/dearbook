894dd1d3783be44e4010493f592091d9c310e221
zacktagnan <zacktagnan@gmail.com>
2026-04-10 22:41:13 +0200

    fix(bootstrap): trust proxies in app middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -24,6 +24,7 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
 
         //
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         // No vale :: ini
